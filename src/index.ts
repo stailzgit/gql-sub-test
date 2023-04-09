e3955bc3205bbb0615a4b341d3e8a6aebc90303f
stailzgit <77897064+stailzgit@users.noreply.github.com>
2023-04-09 21:36:19 +0300

    little changes db

--- src/index.ts
+++ src/index.ts
@@ -10,30 +10,47 @@ import { PubSub, withFilter } from "graphql-subscriptions";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { typeDefs } from "../typeDefs.js";
+import { v4 as uuidv4 } from "uuid";
 
 const PORT = 4000;
 const pubsub = new PubSub();
 
 const users = [
-  { id: "1", name: "Stas" },
-  { id: "2", name: "Vova" },
+  { id: "11", name: "Stas" },
+  { id: "22", name: "Vova" },
 ];
 
 const games = [
-  { id: 1, userId1: 1, userId2: 2, winnerId: "1" },
-  { id: 2, userId1: 1, userId2: 2, winnerId: "2" },
+  {
+    id: "21",
+    userId1: "11",
+    userId2: "22",
+    winnerId: "11",
+    gameEndScore: 1,
+    userScore1: 0,
+    userScore2: 0,
+  },
+  {
+    id: "22",
+    userId1: "11",
+    userId2: "22",
+    winnerId: "22",
+    gameEndScore: 2,
+    userScore1: 0,
+    userScore2: 0,
+  },
 ];
 
 const rounds = [
-  { gameId: "1", id: "1", user1: "0", user2: "1" },
-  { gameId: "1", id: "2", user1: "0", user2: "0" },
-  { gameId: "1", id: "3", user1: "0", user2: "0" },
-  { gameId: "1", id: "4", user1: "0", user2: "1" },
+  { gameId: "21", id: "1", userChoice1: "0", userChoice2: "1" },
+  { gameId: "21", id: "2", userChoice1: "0", userChoice2: "0" },
+  { gameId: "21", id: "3", userChoice1: "0", userChoice2: "0" },
+  { gameId: "21", id: "4", userChoice1: "0", userChoice2: "1" },
 
-  { gameId: "2", id: "5", user1: "0", user2: "0" },
-  { gameId: "2", id: "6", user1: "0", user2: "2" },
-  { gameId: "2", id: "7", user1: "0", user2: "2" },
-  { gameId: "2", id: "8", user1: "0", user2: "0" },
+  { gameId: "22", id: "5", userChoice1: "0", userChoice2: "0" },
+  { gameId: "22", id: "6", userChoice1: "0", userChoice2: "2" },
+  { gameId: "22", id: "7", userChoice1: "0", userChoice2: "2" },
+  { gameId: "22", id: "8", userChoice1: "0", userChoice2: "0" },
 ];
 
 // Schema definition
@@ -67,14 +84,64 @@ const resolvers = {
       return users;
     },
     getUser: (_, { id }) => {
-      console.log("id", id);
-
-      const result = users.find((user) => user.id == id);
-      console.log("result", result);
-
-      return result;
+      return users.find((user) => user.id == id);
+    },
+    getGamesForUser(_, { id }) {
+      return games.filter(
+        ({ userId1, userId2 }) => userId1 == id || userId2 == id
+      );
+    },
+    getRoundsInGameForUser(_, { gameId, userId }) {
+      return rounds.filter(({ gameId: _gameId }) => _gameId === gameId);
     },
   },
+  Mutation: {
+    createGame: (_, { userId1, userId2 }) => {
+      const newGame = {
+        id: uuidv4(),
+        userId1,
+        userId2,
+        winnerId: null,
+        gameEndScore: 1,
+        userScore1: 0,
+        userScore2: 0,
+      };
+      games.push(newGame);
+      return newGame;
+    },
+    createRound: (_, { gameId }) => {
+      const newRound = {
+        gameId: gameId,
+        id: uuidv4(),
+        userChoice1: null,
+        userChoice2: null,
+      };
+      rounds.push(newRound);
+      return newRound;
+    },
+    userSelectFigure: (_, { gameId, roundId, userId, selectedFigure }) => {
+      const findRoundIndex = rounds.findIndex(
+        ({ gameId: _gameId, id: _id }) => _gameId === gameId && _id === roundId
+      );
+
+      const findGameIndex = games.findIndex(({ id }) => id === gameId);
+
+      if (games[findGameIndex].userId1 === userId) {
+        rounds[findRoundIndex].userChoice1 = selectedFigure;
+      } else if (games[findGameIndex].userId2 === userId) {
+        rounds[findRoundIndex].userChoice2 = selectedFigure;
+      }
+
+      const { userChoice1, userChoice2 } = rounds[findRoundIndex];
+      if (userChoice1 !== null && userChoice2 !== null) {
+        if (userChoice1 === userChoice1) {
+        }
+      }
+
+      return rounds[findRoundIndex];
+    },
+  },
+
   // Mutation: {
   //   updateAgeUser: async (_, { id, newAge }) => {
   //     const findUserIndex = users.findIndex((user) => user.id == id);
@@ -95,6 +162,8 @@ const resolvers = {
   //   },
   // },
 };
+
+const whoWin = () => {};
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
--- typeDefs.js
+++ typeDefs.js
@@ -6,9 +6,13 @@ type User {
     name: String
   }
 
-  # type Game {
-    
-  # }
+  type Game {
+    id: ID, userId1: ID, userId2: ID, winnerId: ID, gameEndScore: 1 userScore1:Int, userScore2: Int
+  }
+
+  type Round {
+    gameId: ID, id: ID, userChoice1: Int, userChoice2: Int
+  }
 
 
   input UserInput {
@@ -20,10 +24,14 @@ type User {
   type Query {
     getAllUsers: [User]
     getUser(id: ID): User
-    # getGamesByUserId(id: ID): [Game]
+    getGamesForUser(id: ID): [Game]
+    getRoundsInGameForUser(gameId: ID, userId: ID): [Round]
   }
 
   type Mutation {
     createUser(input: UserInput): User
+    createGame(userId1: ID, userId2: ID): Game
+    createRound(gameId: ID): Round
+    userSelectFigure(gameId: ID, roundId: ID, userId: ID, selectedFigure: String): Round
   }
 `);
